0b5e8c78761ceed8fbbcf5e7fe740b07a4d25b97
esraa <esraa.20.21.ahmedd@gmail.com>
2022-09-07 08:10:21 +0200

    X-o game

---
+++ main.js
@@ -0,0 +1,205 @@
+let title = document.querySelector('.title');
+
+let turn = 'x';
+let squares = [];
+
+function winner(){
+    for(let i =1 ;i< 10; i++){
+        squares[i] = document.getElementById('item' + i).innerHTML;
+    }
+
+    if(squares[1] === squares[2] && squares[2] === squares[3] && squares[3] != '' ){
+        
+        title.innerHTML=`${squares[1]} winner` ;
+        document.getElementById('item'+ 1).style.background='#000';
+        document.getElementById('item'+ 2).style.background='#000';
+        document.getElementById('item'+ 3).style.background='#000';
+
+        setInterval(function(){title.innerHTML += '.'} ,1000);
+        setTimeout(function(){location.reload()},4000);
+    }
+    else if(squares[4] == squares[5] && squares[5] == squares[6] && squares[5] != '' ){
+        
+        title.innerHTML=`${squares[4]} winner`;
+        document.getElementById('item'+ 4).style.background='#000';
+        document.getElementById('item'+ 5).style.background='#000';
+        document.getElementById('item'+ 6).style.background='#000';
+
+        setInterval(function(){title.innerHTML += '.'} ,1000);
+        setTimeout(function(){location.reload()},4000);
+
+    }
+
+    else if(squares[7] == squares[8] && squares[8] == squares[9] && squares[8] != '' ){
+        
+        title.innerHTML=`${squares[7]} winner`;
+        document.getElementById('item'+ 7).style.background='#000';
+        document.getElementById('item'+ 8).style.background='#000';
+        document.getElementById('item'+ 9).style.background='#000';
+
+        setInterval(function(){title.innerHTML += '.'} ,1000);
+        setTimeout(function(){location.reload()},4000);
+
+    }
+
+
+
+    else if(squares[1] == squares[4] && squares[4] == squares[7] && squares[4] != '' ){
+        
+        title.innerHTML=`${squares[1]} winner`;
+        document.getElementById('item'+ 1).style.background='#000';
+        document.getElementById('item'+ 4).style.background='#000';
+        document.getElementById('item'+ 7).style.background='#000';
+
+        setInterval(function(){title.innerHTML += '.'} ,1000);
+        setTimeout(function(){location.reload()},4000);
+
+    }
+
+    else if(squares[2] == squares[5] && squares[5] == squares[8] && squares[5] != '' ){
+        
+        title.innerHTML=`${squares[2]} winner`;
+        document.getElementById('item'+ 2).style.background='#000';
+        document.getElementById('item'+ 5).style.background='#000';
+        document.getElementById('item'+ 8).style.background='#000';
+
+        setInterval(function(){title.innerHTML += '.'} ,1000);
+        setTimeout(function(){location.reload()},4000);
+
+    }
+
+    else if(squares[3] == squares[6] && squares[6] == squares[9] && squares[6] != '' ){
+        
+        title.innerHTML=`${squares[3]} winner`;
+        document.getElementById('item'+ 9).style.background='#000';
+        document.getElementById('item'+ 6).style.background='#000';
+        document.getElementById('item'+ 3).style.background='#000';
+
+        setInterval(function(){title.innerHTML += '.'} ,1000);
+        setTimeout(function(){location.reload()},4000);
+
+    }
+
+
+
+    else if(squares[1] == squares[5] && squares[5] == squares[9] && squares[5] != '' ){
+        
+        title.innerHTML=`${squares[1]} winner`;
+        document.getElementById('item'+ 1).style.background='#000';
+        document.getElementById('item'+ 5).style.background='#000';
+        document.getElementById('item'+ 9).style.background='#000';
+
+        setInterval(function(){title.innerHTML += '.'} ,1000);
+        setTimeout(function(){location.reload()},4000);
+
+    }
+
+    else if(squares[3] == squares[5] && squares[5] == squares[7] && squares[5] != '' ){
+        
+        title.innerHTML=`${squares[3]} winner`;
+        document.getElementById('item'+ 7).style.background='#000';
+        document.getElementById('item'+ 5).style.background='#000';
+        document.getElementById('item'+ 3).style.background='#000';
+
+        setInterval(function(){title.innerHTML += '.'} ,1000);
+        setTimeout(function(){location.reload()},4000);
+
+    }
+
+}
+
+
+//////////////////////////////////..............**********-------another way------**********..............//////////////////////////////////////
+
+/*
+    function end(num1,num2,num3){
+
+        title.innerHTML=`${squares[num1]} winner`;
+        document.getElementById('item'+ num2).style.background='#000';
+        document.getElementById('item'+ num2).style.background='#000';
+        document.getElementById('item'+ num3).style.background='#000';
+
+        setInterval(function(){title.innerHTML += '.'} ,1000);
+        setTimeout(function(){location.reload()},4000);
+
+    }
+
+    
+function winner(){
+    for(let i =1 ;i< 10; i++){
+        squares[i] = document.getElementById('item' + i).innerHTML;
+    }
+
+    if(squares[1] === squares[2] && squares[2] === squares[3] && squares[3] != '' ){
+        
+        end(1,2,3);
+    }
+
+    else if(squares[4] == squares[5] && squares[5] == squares[6] && squares[5] != '' ){
+        
+        end(4,5,6);
+
+    }
+
+    else if(squares[7] == squares[8] && squares[8] == squares[9] && squares[8] != '' ){
+        
+        end(7,8,9);
+
+    }
+
+
+
+    else if(squares[1] == squares[4] && squares[4] == squares[7] && squares[4] != '' ){
+        
+        end(1,4,7);
+
+    }
+
+    else if(squares[2] == squares[5] && squares[5] == squares[8] && squares[5] != '' ){
+        
+        end(2,5,8);
+    }
+
+    else if(squares[3] == squares[6] && squares[6] == squares[9] && squares[6] != '' ){
+        
+        end(3,6,9);
+
+    }
+
+
+
+    else if(squares[1] == squares[5] && squares[5] == squares[9] && squares[5] != '' ){
+        
+        end(1,5,9);
+    }
+
+    else if(squares[3] == squares[5] && squares[5] == squares[7] && squares[5] != '' ){
+        
+        end(3,5,7);
+
+    }
+
+
+
+ */
+
+
+
+
+
+
+
+function game(id){
+    let element =document.getElementById(id);
+
+    if(turn === 'x' && element.innerHTML ==''){
+        element.innerHTML = 'X';
+        turn = 'o';
+        title.innerHTML = 'O';
+    }else if(turn === 'o' && element.innerHTML ==''){
+        element.innerHTML = 'O';
+        turn = 'x';
+        title.innerHTML = 'X';
+    }
+    winner();
+}
